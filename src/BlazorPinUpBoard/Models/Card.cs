253f1555415f77a9ceb236ecda1c480675a191e4
Jarek Danielak <danielak.jarek@gmail.com>
2020-03-26 12:49:43 +0100

    Initial commit

---
+++ src/BlazorPinUpBoard/Models/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPinUpBoard
+{
+    public class Card
+    {
+        private static int _count = 0;
+
+        public bool IsInitialized { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public string TitleElementId { get; private set; }
+
+        public string TextElementId { get; private set; }
+
+        public Card()
+        {
+            TitleElementId = $"card-title-{_count}";
+            TextElementId = $"card-text-{_count}";
+            _count++;
+        }        
+    }
+}
---
+++ src/BlazorPinUpBoard/wwwroot/js/main.js
@@ -0,0 +1,35 @@
+﻿function enableEditingTitle(id, textId) {
+    const target = document.getElementById(id);
+    target.readOnly = false;
+    target.focus();
+
+    target.addEventListener("keyup", e => {
+        if (event.isComposing || event.keyCode === 13) {
+            disableEditingElement(id);
+            const textTarget = document.getElementById(textId);
+            if (!textTarget.value) {
+                enableEditingText(textId);
+            }
+        }
+    });
+
+    target.addEventListener("focusout", e => {
+        disableEditingElement(id);
+    })
+}
+
+function enableEditingText(id) {
+    const target = document.getElementById(id);
+    target.readOnly = false;
+    target.focus();
+
+    target.addEventListener("focusout", e => {
+        disableEditingElement(id);
+    })
+}
+
+function disableEditingElement(id) {
+    const target = document.getElementById(id);
+    target.readOnly = true;
+    target.blur();
+}
